b5919ef4edfb527ebcf8621ccaa87e7bce237b7f
Steven Blowers <steven@shdblowers.com>
2019-12-12 22:42:35 +0000

    spaces not tabs

--- Matrices/Matrix3x3.h
+++ Matrices/Matrix3x3.h
@@ -2,12 +2,13 @@
 class Matrix3x3
 {
 private:
-	int m[3][3] = { {0,0,0},{0,0,0},{0,0,0} };
+    int m[3][3] = { {0,0,0},{0,0,0},{0,0,0} };
 
 public:
-	Matrix3x3() = default;
-	Matrix3x3(int mm[3][3]);
-	void interchange(int, int);
-	void print();
+    Matrix3x3() = default;
+    Matrix3x3(int mm[3][3]);
+
+    void interchange(int, int);
+    void print();
 };
 
